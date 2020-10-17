1a16ee7007761a488acfb59dfcef8a0acc6eefdc
Irio Borges <iriormb@gmail.com>
2020-10-17 11:23:36 -0300

    Requisito [id=1] implementado - servidor enviando e recebendo mensagens não persistentes

--- Servidor.c
+++ Servidor.c
@@ -4,27 +4,58 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#include <pthread.h>
-#include <sys/socket.h>
+#define PORT 8080
 
-int main(int argc, char **argv){   
-    int s, client_s;
-    struct sockaddr_in self, client;
-    int addrlen = sizeof(client);
-    char msg_write[100], msg_read[100];
-    s = socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&self, sizeof(self));
-    self.sin_family = AF_INET;
-    self.sin_port = htons(80);
-    self.sin_addr.s_addr = INADDR_ANY;
-    bind(s, (struct sockaddr*)&self, sizeof(self));
-    listen(s, 5);
-    while (1) {
-        client_s = accept(s, (struct sockaddr*)&client, &addrlen);
-        do {
-            read (client_s, msg_read, MAXBUF);
-            write (client_s, msg_read, strlen(msg_read)+1);
-        } while (strcmp(msg_read,"bye"));
-            close(client_s);
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "<html><head><title>teste</title><body></body></html>";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        valread = read(new_socket, buffer, 1024);
+        printf("%s\n", buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        printf("Mensagem enviada\n");
+    }
+    return 0;
 }
